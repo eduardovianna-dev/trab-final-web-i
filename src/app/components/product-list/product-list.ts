1d15212ad21ad1bf7159ec99e033b9f926c5392f
William Steffen Alievi <walievi@gmail.com>
2025-12-02 17:43:31 -0300

    Base do Trabalho

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -10,9 +10,9 @@ import { NgFor } from '@angular/common';
   styleUrl: './product-list.css'
 })
 export class ProductList {
-  products = [
-    { id: 1, name: 'Produto A', price: 19.99, imageUrl: 'https://picsum.photos/seed/1/400/300' },
-    { id: 2, name: 'Produto B', price: 29.99, imageUrl: 'https://picsum.photos/seed/2/400/300' },
-    { id: 3, name: 'Produto C', price: 39.99, imageUrl: 'https://picsum.photos/seed/3/400/300' }
-  ];
+  // TODO: Aluno
+  // 1. Crie uma lista (array) de produtos.
+  //    Cada produto deve ter: id, name, price, e imageUrl.
+  //    Ex: { id: 1, name: 'Nome do Produto', price: 99.99, imageUrl: 'url_d-imagem' }
+  products = [];
 }
